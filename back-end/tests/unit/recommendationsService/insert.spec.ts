8a92f09e04fe237dbd855346bb49a50d27795a26
andreseichi <andre_seichi@hotmail.com>
2022-09-23 01:21:03 -0300

    unit test for insert service

---
+++ back-end/tests/unit/recommendationsService/insert.spec.ts
@@ -0,0 +1,58 @@
+import { prisma } from "../../../src/database";
+import { recommendationRepository } from "../../../src/repositories/recommendationRepository";
+import {
+  CreateRecommendationData,
+  recommendationService,
+} from "../../../src/services/recommendationsService";
+
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations RESTART IDENTITY CASCADE`;
+
+  jest.resetAllMocks();
+});
+
+describe("Insert service", () => {
+  it("should insert a new recommendation", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockResolvedValueOnce(null);
+    jest
+      .spyOn(recommendationRepository, "create")
+      .mockImplementationOnce((): any => {});
+
+    const createRecommendationData: CreateRecommendationData = {
+      name: "Test",
+      youtubeLink: "https://www.youtube.com/watch?v=1",
+    };
+
+    await recommendationService.insert(createRecommendationData);
+
+    expect(recommendationRepository.create).toHaveBeenCalledTimes(1);
+  });
+
+  it("should throw an error if the name is already in use", async () => {
+    jest.spyOn(recommendationRepository, "findByName").mockResolvedValueOnce({
+      id: 1,
+      name: "Test",
+      youtubeLink: "https://www.youtube.com/watch?v=1",
+      score: 0,
+    });
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockResolvedValueOnce({} as any);
+
+    const createRecommendationData: CreateRecommendationData = {
+      name: "Test",
+      youtubeLink: "https://www.youtube.com/watch?v=1",
+    };
+    const expectedThrow = {
+      message: "Recommendations names must be unique",
+      type: "conflict",
+    };
+
+    const promise = recommendationService.insert(createRecommendationData);
+
+    expect(promise).rejects.toEqual(expectedThrow);
+    expect(recommendationRepository.create).toHaveBeenCalledTimes(0);
+  });
+});
