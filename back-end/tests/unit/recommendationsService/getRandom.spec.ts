212e67a7b9765dfcd9996fcafa419b25eac7944e
andreseichi <andre_seichi@hotmail.com>
2022-09-23 23:16:28 -0300

    unit test for getRandom service

---
+++ back-end/tests/unit/recommendationsService/getRandom.spec.ts
@@ -0,0 +1,274 @@
+import { recommendationRepository } from "../../../src/repositories/recommendationRepository";
+import { recommendationService } from "../../../src/services/recommendationsService";
+
+beforeEach(async () => {
+  jest.resetAllMocks();
+  jest.resetModules();
+  jest.clearAllMocks();
+});
+
+describe("getRandom service", () => {
+  it("should return a random recommendation with score greater than 10", async () => {
+    const recommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 25,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 15,
+      },
+    ];
+    const expectedRecommendations = [
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 25,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 15,
+      },
+    ];
+    const mockedRandom = 0.9;
+    const scoreFilter = "gt";
+
+    const randomSpy = jest
+      .spyOn(global.Math, "random")
+      .mockReturnValue(mockedRandom);
+    jest
+      .spyOn(recommendationService, "getScoreFilter")
+      .mockReturnValue(scoreFilter);
+    jest
+      .spyOn(recommendationService, "getByScore")
+      .mockResolvedValue(recommendations);
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValue(expectedRecommendations);
+
+    const result = await recommendationService.getRandom();
+
+    expect(randomSpy).toHaveBeenCalledTimes(2);
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+      youtubeLink: expect.any(String),
+      score: expect.any(Number),
+    });
+    expect(result.score).toBeGreaterThan(10);
+    expect(recommendationRepository.findAll).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return a random recommendation with score less than or equal to 10", async () => {
+    const recommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 25,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 15,
+      },
+    ];
+    const expectedRecommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0,
+      },
+    ];
+    const mockedRandom = 0.2;
+    const scoreFilter = "lte";
+
+    const randomSpy = jest
+      .spyOn(global.Math, "random")
+      .mockReturnValue(mockedRandom);
+    jest
+      .spyOn(recommendationService, "getScoreFilter")
+      .mockReturnValue(scoreFilter);
+    jest
+      .spyOn(recommendationService, "getByScore")
+      .mockResolvedValue(recommendations);
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValue(expectedRecommendations);
+
+    const result = await recommendationService.getRandom();
+
+    expect(randomSpy).toHaveBeenCalledTimes(2);
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+      youtubeLink: expect.any(String),
+      score: expect.any(Number),
+    });
+    expect(result.score).toBeLessThanOrEqual(10);
+    expect(recommendationRepository.findAll).toHaveBeenCalledTimes(1);
+  });
+
+  it("should return a random recommendation if there are no recommendations with score greater than 10", async () => {
+    const recommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 5,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 10,
+      },
+    ];
+    const expectedRecommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 0,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 5,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 10,
+      },
+    ];
+    const mockedRandom = 0.9;
+    const scoreFilter = "gt";
+
+    const randomSpy = jest
+      .spyOn(global.Math, "random")
+      .mockReturnValue(mockedRandom);
+    jest
+      .spyOn(recommendationService, "getScoreFilter")
+      .mockReturnValue(scoreFilter);
+    jest
+      .spyOn(recommendationService, "getByScore")
+      .mockResolvedValue(recommendations);
+    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce([]);
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValueOnce(expectedRecommendations);
+
+    const result = await recommendationService.getRandom();
+
+    expect(randomSpy).toHaveBeenCalledTimes(2);
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+      youtubeLink: expect.any(String),
+      score: expect.any(Number),
+    });
+    expect(result.score).toBeLessThanOrEqual(10);
+    expect(recommendationRepository.findAll).toHaveBeenCalledTimes(2);
+  });
+
+  it("should return a random recommendation if there are no recommendations with score less than or equal to 10", async () => {
+    const recommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 15,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 20,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 25,
+      },
+    ];
+    const expectedRecommendations = [
+      {
+        id: 1,
+        name: "Test",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 15,
+      },
+      {
+        id: 2,
+        name: "Test 2",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 20,
+      },
+      {
+        id: 3,
+        name: "Test 3",
+        youtubeLink: "https://www.youtube.com/watch?v=1",
+        score: 25,
+      },
+    ];
+    const mockedRandom = 0.2;
+    const scoreFilter = "lte";
+
+    const randomSpy = jest
+      .spyOn(global.Math, "random")
+      .mockReturnValue(mockedRandom);
+    jest
+      .spyOn(recommendationService, "getScoreFilter")
+      .mockReturnValue(scoreFilter);
+    jest
+      .spyOn(recommendationService, "getByScore")
+      .mockResolvedValue(recommendations);
+    jest.spyOn(recommendationRepository, "findAll").mockResolvedValueOnce([]);
+    jest
+      .spyOn(recommendationRepository, "findAll")
+      .mockResolvedValueOnce(expectedRecommendations);
+
+    const result = await recommendationService.getRandom();
+
+    expect(randomSpy).toHaveBeenCalledTimes(2);
+    expect(result).toEqual({
+      id: expect.any(Number),
+      name: expect.any(String),
+      youtubeLink: expect.any(String),
+      score: expect.any(Number),
+    });
+    expect(result.score).toBeGreaterThan(10);
+    expect(recommendationRepository.findAll).toHaveBeenCalledTimes(2);
+  });
+});
