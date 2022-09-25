181a575464bfe74d15f0d0de888a83adfbdeccb5
andreseichi <andre_seichi@hotmail.com>
2022-09-25 09:56:40 -0300

    seed db

---
+++ back-end/prisma/seed.ts
@@ -0,0 +1,26 @@
+import { prisma } from "../src/database";
+
+import recommendationBodyFactory from "../tests/factories/recommendationBodyFactory";
+
+async function main() {
+  await prisma.recommendation.createMany({
+    data: [
+      recommendationBodyFactory(),
+      recommendationBodyFactory(),
+      recommendationBodyFactory(),
+      recommendationBodyFactory(),
+      recommendationBodyFactory(),
+      recommendationBodyFactory(),
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
