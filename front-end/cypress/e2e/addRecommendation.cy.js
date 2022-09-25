e426f7ca63c163ad300eed83b9f33991a2d819e4
andreseichi <andre_seichi@hotmail.com>
2022-09-25 10:56:57 -0300

    e2e test for not add new recommendation

--- front-end/cypress/e2e/addRecommendation.cy.js
+++ front-end/cypress/e2e/addRecommendation.cy.js
@@ -32,4 +32,51 @@ describe("Add recommendation ", () => {
 
     cy.get("[data-cy=recommendation]").should("have.length", 1);
   });
+
+  it("should not add a new recommendation with repeated name", () => {
+    cy.visit(Cypress.env("APP_URL"));
+    cy.intercept(
+      "GET",
+      Cypress.env("REACT_APP_API_BASE_URL") + "/recommendations"
+    ).as("getRecommendations");
+    cy.wait("@getRecommendations", { timeout: 10000 });
+
+    cy.get("[data-cy=name-input]").type("Recommendation video name");
+    cy.get("[data-cy=link-input]").type(
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    );
+    cy.get("[data-cy=submit-button]").click();
+
+    cy.intercept(
+      "GET",
+      Cypress.env("REACT_APP_API_BASE_URL") + "/recommendations"
+    ).as("getRecommendations");
+
+    cy.wait("@getRecommendations").then((interception) => {
+      expect(interception.response.body).to.have.lengthOf(1);
+    });
+
+    cy.get("[data-cy=recommendation]").should("have.length", 1);
+
+    cy.get("[data-cy=name-input]").type("Recommendation video name");
+    cy.get("[data-cy=link-input]").type(
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    );
+    cy.get("[data-cy=submit-button]").click();
+
+    cy.intercept(
+      "GET",
+      Cypress.env("REACT_APP_API_BASE_URL") + "/recommendations"
+    ).as("getRecommendations");
+
+    cy.wait("@getRecommendations").then((interception) => {
+      expect(interception.response.body).to.have.lengthOf(1);
+    });
+
+    cy.get("[data-cy=recommendation]").should("have.length", 1);
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Error creating recommendation!");
+    });
+  });
 });
