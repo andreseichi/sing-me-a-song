8dd80940d31859d98e65c063234a2d7158bc10e9
andreseichi <andre_seichi@hotmail.com>
2022-09-25 10:34:37 -0300

    e2e test for create recommendation

--- front-end/cypress/e2e/addRecommendation.cy.js
+++ front-end/cypress/e2e/addRecommendation.cy.js
@@ -1,10 +1,35 @@
-describe("empty spec", () => {
-  it("passes", () => {
+Cypress.Commands.add("resetDatabase", () => {
+  cy.request("POST", Cypress.env("REACT_APP_API_BASE_URL") + "/reset-database");
+});
+
+beforeEach(() => {
+  cy.resetDatabase();
+});
+
+describe("Add recommendation ", () => {
+  it("should add a new recommendation", () => {
     cy.visit(Cypress.env("APP_URL"));
     cy.intercept(
       "GET",
       Cypress.env("REACT_APP_API_BASE_URL") + "/recommendations"
     ).as("getRecommendations");
-    cy.wait("@getRecommendations");
+    cy.wait("@getRecommendations", { timeout: 10000 });
+
+    cy.get("[data-cy=name-input]").type("Recommendation video name");
+    cy.get("[data-cy=link-input]").type(
+      "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    );
+    cy.get("[data-cy=submit-button]").click();
+
+    cy.intercept(
+      "GET",
+      Cypress.env("REACT_APP_API_BASE_URL") + "/recommendations"
+    ).as("getRecommendations");
+
+    cy.wait("@getRecommendations").then((interception) => {
+      expect(interception.response.body).to.have.lengthOf(1);
+    });
+
+    cy.get("[data-cy=recommendation]").should("have.length", 1);
   });
 });
